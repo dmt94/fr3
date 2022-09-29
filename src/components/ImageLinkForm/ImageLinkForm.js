58c26f879e14ab371a187c4baa79783af7dd9f06
Daevah Taylaran <daevtayl@gmail.com>
2022-09-29 15:43:37 -0700

    cleaned up search bar

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -3,7 +3,7 @@ import './ImageLinkForm.css';
 
 const ImageLinkForm = () => {
   return (
-    <div>
+    <div className='margin-10px'>
         <p className='f3'>
           {'This Magic Brain will detect faces in your pictures. Give it a try.'}
         </p>
