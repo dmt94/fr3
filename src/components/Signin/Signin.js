44a6e9b3d44eb446444fc51a7fc505e181830946
Daevah Taylaran <daevtayl@gmail.com>
2022-10-11 21:11:36 -0700

    new

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -38,7 +38,7 @@ class Signin extends React.Component {
     const { onRouteChange } = this.props;
     return ( 
       <div className='main-div-sign-in-card'>
-        <article className="b--black-10 mv4 w-100 w-50-m w-25-l mw5 center sign-in-card main-div-sign-in-card">
+        <form className="b--black-10 mv4 w-100 w-50-m w-25-l mw5 center sign-in-card main-div-sign-in-card">
           <main className="pa4 black-80">
             <div className="measure">
               <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
@@ -89,7 +89,7 @@ class Signin extends React.Component {
               </div>
             </div>
           </main>
-        </article>
+        </form>
       </div>
     );
   }
