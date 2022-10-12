c5e7ac71a7627bca5af6aa5bd2b194af3ea95c4d
Daevah Taylaran <daevtayl@gmail.com>
2022-10-11 19:17:22 -0700

    new

--- src/components/BoundingBox/BoundingBox.jsx
+++ src/components/BoundingBox/BoundingBox.jsx
@@ -5,19 +5,18 @@ const Box = ({ person }) => {
   const { id, name, value, boundingBox } = person;
 
   return (
-    <div className='bounding-box-outer-div' key={id}>
-      <div 
-        className='bounding-box'
-        style={
-          {
-            top: boundingBox.topRow, 
-            right: boundingBox.rightCol,
-            bottom: boundingBox.bottomRow,
-            left: boundingBox.leftCol,
-          }
+    <div 
+      className='bounding-box' 
+      key={id}
+      style={
+        {
+          top: boundingBox.topRow, 
+          right: boundingBox.rightCol,
+          bottom: boundingBox.bottomRow,
+          left: boundingBox.leftCol,
         }
-        >
-      </div>
+      }
+      >
       <p className='celebrity-name'>
         {value > 0.3 ? name : ''}
       </p>
