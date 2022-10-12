deae3c502c67f75bcc6c90a008524a184ba9d233
Daevah Taylaran <daevtayl@gmail.com>
2022-10-12 09:14:48 -0700

    added console.log(res) to check for celebrity_model output

--- src/App.js
+++ src/App.js
@@ -116,7 +116,7 @@ class App extends Component {
               this.setState(Object.assign(this.state.user, { entries: count}))
             })
         }
-        // console.log(res.outputs[0].data.regions);
+        console.log(res.outputs[0].data.regions);
         this.displayFaceBox(this.calculateFaceLocation(res));
     }).catch(error => console.log('error', error));
       //END OF CLARIFAI REST API
