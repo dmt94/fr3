fe642f90cc17b7b5f784f022af154cd9f1498483
Daevah Taylaran <daevtayl@gmail.com>
2022-10-12 20:00:50 -0700

    change entry infos

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Navigation.css'
 
 const Navigation = ({onRouteChange, isSignedIn}) => {
     if (isSignedIn) {
@@ -6,14 +7,14 @@ const Navigation = ({onRouteChange, isSignedIn}) => {
         <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
           <p 
             onClick={() => onRouteChange('signout')}
-            className='f3 link dim white underline pa3 pointer'>
+            className='f3 link dim white pa3 pointer signin-out'>
                Sign Out </p>
       </nav>
       )
    
     } else {
       return (
-        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+        <nav className='navbar' style={{display: 'flex', justifyContent: 'flex-end'}}>
           <p 
             onClick={() => onRouteChange('signin')}
             className='f3 link dim white underline pa3 pointer'> Sign In </p>
